694fd5ae6df0881db066f95b926da2b1636e5409
Patriciasadat <sanahoseini83@gmail.com>
2025-01-29 22:47:03 +0330

    Update views.py

--- course_management/schedule/views.py
+++ course_management/schedule/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Course
 
-# Create your views here.
+def weekly_schedule(request):
+    student_courses = Course.objects.filter(student=request.user)
+    schedule = {}
+    for course in student_courses:
+        if course.day_of_week not in schedule:
+            schedule[course.day_of_week] = []
+        schedule[course.day_of_week].append(course)
+    return render(request, 'schedule/weekly_schedule.html', {'schedule': schedule})
