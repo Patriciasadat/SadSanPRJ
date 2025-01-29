e7952d4bc6ead71baac60cd858f3aa61787a9084
Patriciasadat <sanahoseini83@gmail.com>
2025-01-29 22:45:19 +0330

    Update models.py

--- course_management/schedule/models.py
+++ course_management/schedule/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    day_of_week = models.CharField(max_length=10)  # e.g., 'Monday'
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
