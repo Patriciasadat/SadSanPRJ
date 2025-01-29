9603be2db75437fbc861c89903be5a183a3f4743
sadra81 <64496573+sadra81@users.noreply.github.com>
2025-01-30 00:09:21 +0330

    Update forms.py

--- course_management/management/forms.py
+++ course_management/management/forms.py
@@ -4,4 +4,7 @@ from .models import Course
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ['name', 'code', 'units', 'department', 'instructor', 'days', 'time', 'exam_datetime']
+        fields = ['name', 'code', 'units', 'department', 'instructor', 'days', 'start_time', 'end_time', 'exam_datetime']
+    
+    # Exclude the student field from the form
+    # student = forms.ModelChoiceField(queryset=CustomUser.objects.filter(is_student=True), required=False) # optional, if you want to allow selecting a student explicitly
