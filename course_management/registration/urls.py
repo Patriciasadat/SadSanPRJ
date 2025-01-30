4c0632324f2e1fdcd19d39a18f2f9c6562bc0728
sadra81 <64496573+sadra81@users.noreply.github.com>
2025-01-30 14:41:55 +0330

    Update urls.py

--- course_management/registration/urls.py
+++ course_management/registration/urls.py
@@ -1,9 +1,13 @@
+# registration/urls.py
+
 from django.urls import path
 from .views import register
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
+app_name = 'registration'  # Add a namespace for this app
 
 urlpatterns = [
     path("register/", register, name="register"),
-    path("login/", LoginView.as_view(template_name="login.html"), name="login")
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),  # Added logout URL
 ]
