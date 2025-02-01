ba35e138481422706e7983df168988db2ab8d35f
sadra81 <64496573+sadra81@users.noreply.github.com>
2025-02-02 01:59:30 +0330

    Update forms.py
    fixed an issue where the user couldn't use pickers to pick date and time

--- course_management/management/forms.py
+++ course_management/management/forms.py
@@ -4,7 +4,8 @@ from .models import Course
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ['name', 'code', 'units', 'department', 'instructor', 'days', 'start_time', 'end_time', 'exam_datetime', 'capacity']
-
-    # Exclude the student field from the form
-    # student = forms.ModelChoiceField(queryset=CustomUser.objects.filter(is_student=True), required=False) # optional, if you want to allow selecting a student explicitly
+        fields = ['name', 'code', 'units', 'department', 'instructor', 'capacity', 'start_time', 'end_time', 'exam_datetime','days']
+    
+    start_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+    end_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+    exam_datetime = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
