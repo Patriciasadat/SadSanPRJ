2aaab34f37f6fac51136c0fdec060eb4379510e7
sadra81 <64496573+sadra81@users.noreply.github.com>
2025-02-02 12:15:18 +0330

    Update forms.py
    gave a placeholder for days and made sure that you can also use the default end time

--- course_management/management/forms.py
+++ course_management/management/forms.py
@@ -7,5 +7,6 @@ class CourseForm(forms.ModelForm):
         fields = ['name', 'code', 'units', 'department', 'instructor', 'capacity', 'start_time', 'end_time', 'exam_datetime','days']
     
     start_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
-    end_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+    end_time = forms.TimeField(required= False ,widget=forms.TimeInput(attrs={'type': 'time'}))
     exam_datetime = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+    days = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'e.g., Mon, Wed, Fri'}))
