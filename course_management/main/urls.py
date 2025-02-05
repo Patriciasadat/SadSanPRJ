845da7570e93262e3c9d0ff75630231e1d93ef3f
sadra81 <64496573+sadra81@users.noreply.github.com>
2025-02-05 13:57:11 +0330

    Update urls.py
    made sure the urls work properly

--- course_management/main/urls.py
+++ course_management/main/urls.py
@@ -1,3 +1,4 @@
+# In main/urls.py
 from django.urls import path
 from . import views
 
@@ -6,5 +7,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('profile/', views.update_profile, name='update_profile'),
-    path('enroll/<int:course_id>/', views.enroll_in_course, name='enroll_in_course'),
+    path('enroll/<str:course_code>/', views.enroll_in_course, name='enroll_in_course'),
+    path('drop_course/<str:course_code>/', views.drop_course, name='drop_course'),  # Add this line
 ]
